65f99601e61bd9e63d3debfe1a5385fd4a712fdc
Hashane <jhashane@gmail.com>
2025-04-19 00:30:26 +0530

    Enhancement: Updated chat endpoint to  create a conversation if not exists.

--- app/api/chatbot.py
+++ app/api/chatbot.py
@@ -4,6 +4,7 @@ from app.auth.schemas import User
 from sqlalchemy.orm import Session
 from app.chatbot.engine import get_similar_response
 from app.chatbot.schemas import ChatResponse, ChatRequest
+from app.db.models import Conversation
 
 router = APIRouter(
     prefix="/chatbot",
@@ -12,7 +13,30 @@ router = APIRouter(
 
 
 @router.post("/chat", response_model=ChatResponse)
-def chat_endpoint(request: ChatRequest, current_user: User = Depends(get_current_user),
-                  db: Session = Depends(get_session_local)):
-    reply = get_similar_response(request.text, current_user, request.conversation_id, db)
-    return ChatResponse(reply=reply)
+def chat_endpoint(
+    request: ChatRequest,
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_session_local),
+):
+    conversation_id = request.conversation_id
+
+    # If no conversation_id, create a new conversation
+    if not conversation_id:
+        new_convo = Conversation(
+            user_id=current_user.id,
+            title=request.text[:30]
+        )
+        db.add(new_convo)
+        db.commit()
+        db.refresh(new_convo)
+        conversation_id = new_convo.id
+
+    # Get the bot's reply
+    reply,conversation_id = get_similar_response(
+        request.text, current_user, conversation_id, db
+    )
+
+    return {
+        "reply": reply,
+        "conversation_id": conversation_id
+    }
